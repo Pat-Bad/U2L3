98ea6a5b69e4127d5d531bc6abeefd5543658853
Patricia <patricia.badji89@gmail.com>
2025-01-15 16:42:52 +0100

    Update Negozio.java

--- src/EsercizioStream/Negozio.java
+++ src/EsercizioStream/Negozio.java
@@ -6,21 +6,21 @@ import java.util.stream.Stream;
 
 public class Negozio {
     public static void main (String[] args) {
-        Product book1 = new Product(12367008009l, "Wild Heart", "Books", 29.99);
-        System.out.println(book1.toString());
-        Product book2 = new Product (2222222l, "Capitan Mutanda", "Books", 12.99 );
-        Product book3 = new Product (6666666l, "The Oracle", "Books", 126.66);
+        Product book1 = new Product(12367008009L, "Wild Heart", "Books", 29.99);
+        System.out.println(book1);
+        Product book2 = new Product (2222222L, "Capitan Mutanda", "Books", 12.99 );
+        Product book3 = new Product (6666666L, "The Oracle", "Books", 126.66);
 
-        Product baby1 = new Product(9876543213200l, "Biberon", "Baby", 16.99);
-        System.out.println(baby1.toString());
-        Product baby2 = new Product(456789456l, "Pannolini", "Baby", 34.99);
-        Product baby3 = new Product (541236987l, "Tutina", "Baby", 25.99);
+        Product baby1 = new Product(9876543213200L, "Biberon", "Baby", 16.99);
+        System.out.println(baby1);
+        Product baby2 = new Product(456789456L, "Pannolini", "Baby", 34.99);
+        Product baby3 = new Product (541236987L, "Tutina", "Baby", 25.99);
 
-        Customer customer1 = new Customer(85214790l, "Patricia", 1);
-        System.out.println(customer1.toString());
+        Customer customer1 = new Customer(85214790L, "Patricia", 1);
+        System.out.println(customer1);
 
-        Customer customer2 = new Customer(456321111l, "Barbara", 2);
-        System.out.println(customer2.toString());
+        Customer customer2 = new Customer(456321111L, "Barbara", 2);
+        System.out.println(customer2);
 
         //Creo la lista da passare all'ordine
         List<Product> products = new ArrayList<>();
@@ -39,11 +39,11 @@ public class Negozio {
 
 
 
-        Order ordineProva = new Order(4444444l, "processing", products, customer1);
-        System.out.println(ordineProva.toString());
+        Order ordineProva = new Order(4444444L, "processing", products, customer1);
+        System.out.println(ordineProva);
 
-        Order ordineProva2 = new Order (5555555l, "out for delivery", products2, customer2);
-        System.out.println(ordineProva2.toString());
+        Order ordineProva2 = new Order (5555555L, "out for delivery", products2, customer2);
+        System.out.println(ordineProva2);
 
         //ESERCIZIO 1
 
@@ -51,7 +51,7 @@ public class Negozio {
 
         List<Product> libriSopraCento = products.stream()
                 .filter(product -> "Books".equals(product.getCategory()) && product.getPrice() > 100)
-                        .collect(Collectors.toList());
+                        .toList();
 
         libriSopraCento.forEach(product -> System.out.println("Prodotto della categoria Book con prezzo sopra i 100 euro: " + product.getName()));
 
@@ -60,7 +60,7 @@ public class Negozio {
 
         List<Product> articoliBambino = products2.stream()
                 .filter(product -> "Baby".equals(product.getCategory()))
-                .collect(Collectors.toList());
+                .toList();
         articoliBambino.forEach(product -> System.out.println("Prodotto della categoria Baby: " + product.getName()));
 
 
